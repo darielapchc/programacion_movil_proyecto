950a276453f2852984eccc69eded5d9b53386189
Isis Pacheco <darielapchc@gmail.com>
2026-08-02 10:53:12 -0600

    cambio

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -9,7 +9,6 @@ class MenuScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-
       appBar: AppBar(
         title: const Text("Menú Principal"),
       ),
