8bdeb98fa6e372367865725eb8322e8fceffef91
LiaJael <liamaradiaga2007@gmail.com>
2026-08-24 18:45:13 -0600

    feat: agregar propiedades y método factory a la clase Categorias

--- lib/models/categorias.dart
+++ lib/models/categorias.dart
@@ -1,12 +1,28 @@
+import 'dart:js_interop';
+import 'package:flutter/material.dart';
+
 class Categorias {
   final int idCategoria;
   final String nombre;
   final String descripcion;
+  final String icono;
+  final bool activo;
 
   Categorias({
     required this.idCategoria,
     required this.nombre,
     required this.descripcion,
+    required this.icono,
+    required this.activo,
   });
 
+  factory Categorias.fromJson(Map<String, dynamic> json) {
+    return Categorias(
+      idCategoria: json['idCategoria'], 
+      nombre: json['nombre'],
+      descripcion: json['descripcion'] ?? "",
+      icono: json['icono'] ?? "",
+      activo: json['activo'] ?? true,
+    );
+  }
 }
